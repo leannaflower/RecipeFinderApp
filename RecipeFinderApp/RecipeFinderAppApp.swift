1806d0eb5ac3564d31bf11f9d72470972fb2e226
Leanna Fowler <143443662+leannaflower@users.noreply.github.com>
2026-03-06 09:57:59 -0500

    Initial Commit

---
+++ RecipeFinderApp/RecipeFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderAppApp.swift
+//  RecipeFinderApp
+//
+//  Created by Leanna Fowler on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
